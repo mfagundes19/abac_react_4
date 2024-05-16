f5dfe39fb3cad6a8dd444ff1cacc30077c3b79f3
Marcelo <desenvolvimento2@bcseventos.com.br>
2024-05-16 09:10:35 -0300

    Alteração de Component, criação de TextControl

---
+++ src/Component/TextControl/TextControl.js
@@ -0,0 +1,63 @@
+function getDateTime() {
+    const date = new Date();
+    return (date.toLocaleDateString()+' '+date.getHours()+':'+date.getMinutes()+':'+date.getSeconds());
+}
+
+
+
+const TextControl = (props) => {
+    switch(props.type){
+        case 'h1':
+            return(
+                <h1>{props.name} - {getDateTime()}</h1>
+            )    
+            break;
+        case 'h2':
+            return(
+                <h2>{props.name} - {getDateTime()}</h2>
+            )    
+            break;
+        case 'h3':
+            return(
+                <h3>{props.name} - {getDateTime()}</h3>
+            )    
+            break;
+        case 'h4':
+            return(
+                <h4>{props.name} - {getDateTime()}</h4>
+            )    
+            break;
+        case 'h5':
+                return(
+                    <h5>{props.name} - {getDateTime()}</h5>
+                )    
+                break;
+        case 'h6':
+            return(
+                <h6>{props.name} - {getDateTime()}</h6>
+            )    
+            break;
+        case 'span':
+            return(
+                <span>{props.name} - {getDateTime()}</span>
+            )  
+            break;
+        case 'span-bold':
+            return(
+                <span><b>{props.name} - {getDateTime()}</b></span>
+            )  
+            break;
+        case 'span-italic':
+            return(
+                <span><i>{props.name} - {getDateTime()}</i></span>
+            )  
+            break;
+        case 'span-color':
+            return(
+                <span style={{color: props.color}}>{props.name} - {getDateTime()}</span>
+            )  
+            break;
+    }
+}
+
+export default TextControl;
