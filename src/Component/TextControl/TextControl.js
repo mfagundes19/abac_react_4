4269b989c2d9e66f7e1d079944443a9ec4911ccd
Marcelo <desenvolvimento2@bcseventos.com.br>
2024-05-16 09:11:22 -0300

    Correção

--- src/Component/TextControl/TextControl.js
+++ src/Component/TextControl/TextControl.js
@@ -1,6 +1,6 @@
 function getDateTime() {
     const date = new Date();
-    return (date.toLocaleDateString()+' '+date.getHours()+':'+date.getMinutes()+':'+date.getSeconds());
+    return (date.getHours()+':'+date.getMinutes()+':'+date.getSeconds()+' '+date.toLocaleDateString());
 }
 
 
